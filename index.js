ea22d364b6c845e8e608217f999fea2b2cf7ea1e
James <jamonjoroge816@gmail.com>
2024-03-22 15:56:19 +0300

    Control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(somenumber){
   // Write your code here!
+  let result;
+
+  if (somenumber <= 400){
+    result='This one is on me!';
+
+  }else if (somenumber > 400 && somenumber <= 2000){
+    result ="That will be twenty bucks.";
+  }else if (somenumber > 2000 && somenumber <= 2500){
+    result="I will gladly take your thirty bucks.";
+  }else if (somenumber > 2500) {
+    result="No can do.";
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  //Write your code here!
+  return city === 'NYC'? "Ok, sounds good." : "No go.";
+
+
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case "generous":
+      return "Thank you so much."
+    case "not as generous":
+      return "Thank you."
+    default:
+      return "Bye."
+  }
 }
